e1f940faf4ca4add3417a4a310345b1711426eb9
ra2457076 <henrique.dornelas@unesp.br>
2025-03-14 20:18:29 -0300

    adicionando a conversão para Kelvin

--- teste.py
+++ teste.py
@@ -1,4 +1,9 @@
 c = float(input("Digite a temperatura em Celsius: "))
+print("=== Convertendo temperatura em graus Celsius para Farenheit e Kelvin ===")
 
 f = (c * 9/5) + 32
-print(f"Temperatura em Farenheit: {f}")
+k = c + 273.15
+
+print(f"\nTemperatura em Celsius: {c} °C")
+print(f"Temperatura em Farenheit: {f} °F")
+print(f"Temperatura em Kelvin: {k} K")
